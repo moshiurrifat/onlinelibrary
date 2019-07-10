23bb9c087aeddd48d38971bed99270d6b1b8a95c
Moshiur Rifat <34037996+moshiurrifat@users.noreply.github.com>
2019-07-10 10:27:05 +0600

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" type="text/css" href="login.css">
+        
+		<title>Profile</title>
+	</head>
+	<body>
+		<nav>
+				<ul class="nav-links">
+					<li><a class="nav-link" href="index.php">Log Out</a></li>
+				</ul>
+			</nav>
+
+
+		<h1>Welcome to my profile</h1>
+
+	</body>
+</html>
